1db97111312293758f91bcf960b6a4581addbd62
jatin1322 <75074656+jatin1322@users.noreply.github.com>
2022-06-04 13:33:03 +0530

    Time: 164 ms (16.08%), Space: 41.4 MB (40.33%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -1,24 +1,26 @@
-class Solution 
-{
+class Solution {
 public:
-    int subarraySum(vector<int>& nums, int k) 
-    {
-        unordered_map<int,int> mp;
-        mp[0] = 1;
-        int sum = 0;
-        int ans = 0;
-        for(int i = 0;i<nums.size();i++)
-        {
-            sum = sum + nums[i];
-            int req =  sum - k;
-            if(mp.find(req)!=mp.end())
-            {
-                ans+=mp[req]; 
+    int subarraySum(vector<int>& nums, int k) {
+        //here we going to use map for prefix sum and their frequency
+       unordered_map<int,int>map;
+       int pre_sum=0;
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+            pre_sum+=nums[i];
+            if(pre_sum==k){
+                count++;
             }
+            if(map.find(pre_sum-k)!=map.end()){
+                 count+=map[pre_sum-k];//we increase count as many time as many rime                       //pre su comes
+            }
+            map[pre_sum]++;
             
-            mp[sum]+=1;
+            
         }
+        return count;
         
-        return(ans);
+        
+        
+        
     }
 };
