d8143a284c6e9405096f7bf420d85dbe0d47916d
jatin1322 <75074656+jatin1322@users.noreply.github.com>
2022-06-04 20:44:55 +0530

    Time: 0 ms (100.00%), Space: 7 MB (31.36%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -1,70 +1,50 @@
 class Solution {
 public:
-    void ab(vector<int> &v,int a,int b,int c,int d,vector<vector<int>>& matrix,int & ans,int n){
-        if(ans<=n){  v.push_back(matrix[a][c]); ans++;   }
-       
-        for(int i=c+1; i<d ; i++){
-         if(ans<=n)  { v.push_back(matrix[a][i]); ans++;}
-            
-        }
-       
-         if(ans<=n && c<d){   v.push_back(matrix[a][d]); ans++;   }
-        for(int i=a+1; i<b ; i++){
-          
-              if(ans<=n)  {  v.push_back(matrix[i][d]); ans++;}
-        }
-         if(ans<=n){     v.push_back(matrix[b][d]); ans++;   }
-       
-         for(int i=d-1; i>=c+1 ; i--){
-           
-              if(ans<=n)  {   v.push_back(matrix[b][i]); ans++;}
-        }
-          if(ans<=n && c<d){     v.push_back(matrix[b][c]); ans++;   }
-         
-         for(int i=b-1; i>=a+1 ; i--){
-            
-              if(ans<=n)  {  v.push_back(matrix[i][c]); ans++;}
-        }
-        
-    }
-   
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int r=matrix.size();
-        int c1=matrix[0].size();
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int total_steps = m * n;
+        vector<int> res;
         
-        int a=0;
-        int b=r-1;
-        int c=0;
-        int d=c1-1;
-        int ans=1;
-        vector<int> v;
-        int n=r*c1;
-        if(r==1 || c1==1){
-            if(r==1 && c1==1){
-                v.push_back(matrix[0][0]);
-                return v;
+        // int k = 0;
+        int first_row = 0;
+        int last_col = n - 1;
+        int last_row = m - 1;
+        int first_col = 0;
+        
+        while (first_row <= last_row && first_col <= last_col) {
+            
+            // traverse upper row.
+            for (int j = first_col; j <= last_col; j++) {
+                res.push_back(matrix[first_row][j]);
             }
-           if(r==1){
-               for(int i=0; i<c1 ; i++){
-                    v.push_back(matrix[0][i]);
-                }
-                return v;
-           } 
-            else{
-                for(int i=0; i<r ; i++){
-                    v.push_back(matrix[i][0]);
-                }
-                return v;
+            
+            // traverse right column.
+            for (int i = first_row + 1; i <= last_row; i++) {
+                res.push_back(matrix[i][last_col]);
             }
+            
+            // traverse bottom row, make sure we won't traverse the same row twice.
+            if (last_row != first_row) {
+                for (int j = last_col - 1; j >= first_col; j--) {
+                    res.push_back(matrix[last_row][j]);
+                }
+            }
+            
+            // traverse left column, make sure we won't traverse the same column twice.
+            if (first_col != last_col) {
+                for (int i = last_row - 1; i > first_row; i--) {
+                    res.push_back(matrix[i][first_col]);
+                }
+            }
+            
+            // update boundary for next spiral matrix
+            first_row++;
+            last_col--;
+            last_row--;
+            first_col++;
         }
-        while(ans<=n){
-            ab(v,a,b,c,d,matrix,ans,n);
-            a++;
-            b--;
-            c++;
-            d--;
-        }
-    
-        return v;
+        
+        return res;
     }
 };
