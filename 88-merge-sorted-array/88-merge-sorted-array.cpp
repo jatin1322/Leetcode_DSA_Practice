10d17557f1c949e334dfb9cb57ece29d941043c6
jatin1322 <75074656+jatin1322@users.noreply.github.com>
2022-06-07 18:48:47 +0530

    Time: 6 ms (29.53%), Space: 9.1 MB (30.68%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i=0;
-        int j=0;
-        vector<int> v;
-        while(i<m && j<n){
-            if(nums1[i]<=nums2[j]){
-                v.push_back(nums1[i]);
-                i++;
+    void merge(vector<int>& n1, int m, vector<int>& n2, int n) {
+        int i=m-1;
+        int j= n-1;
+        int k=n+m-1;
+        while (i>=0 &&j >=0   )
+        {  
+            if(n1[i]<n2[j]){
+                   n1[k]=n2[j];
+                   k--;
+                   j--;
             }
             else{
-                v.push_back(nums2[j]);
-                j++;
+                n1[k]=n1[i];
+               
+                i--;
+                k--;
             }
-        }
-       while(i<m){
-            v.push_back(nums1[i]);
-                i++;
-       }
-         while(j<n){
-            v.push_back(nums2[j]);
-                j++;
-       }
-        for(int i=0; i<m+n ; i++){
-            nums1[i]=v[i];
-        }
+            /* code */
+            
+        }while(i>=0){
+                n1[k--]=n1[i--];
+            }
+            while(j>=0){
+                n1[k--]=n2[j--];
+            }
+        
+       
+       
+        
     }
 };
