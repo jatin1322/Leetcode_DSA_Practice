c3f64c803b367eb082aa46094e243fca1fb0dcd8
jatin1322 <75074656+jatin1322@users.noreply.github.com>
2022-06-23 03:31:45 +0530

    Time: 80 ms (24.44%), Space: 17.8 MB (51.23%) - LeetHub

--- 143-reorder-list/143-reorder-list.cpp
+++ 143-reorder-list/143-reorder-list.cpp
@@ -7,7 +7,7 @@
  *     ListNode(int x) : val(x), next(nullptr) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
- */
+
 class Solution {
 public:
     void reorderList(ListNode* head) {
@@ -31,5 +31,42 @@ public:
                 head -> next = v[b--];          
         }
         head->next=NULL;
+    }
+};
+ */
+class Solution {
+public:
+	// 876. Middle of the Linked List - returns the mid of list using slow-fast pointer approach
+    ListNode* middleNode(ListNode* head) {
+        auto slow = head, fast = head;
+        while(fast && fast -> next)
+            slow = slow -> next,
+            fast = fast -> next -> next;            // fast moves at 2x speed
+        return slow;                                // slow ends up at mid
+    }
+	// 206. Reverse Linked List - reverses and returns the head of reversed list
+    ListNode* reverseList(ListNode* head) {
+        ListNode *prev = nullptr;
+        while(head) {
+            auto nextNode = head -> next;           // store next node before reversing next ptr of cur
+            head -> next = prev;                    // reverse the next ptr to previous node
+            prev = head;                            // update previous node as cur
+            head = nextNode;                        // move to orignal next node
+        }
+        return prev;                                // returns head of reversed list
+    }
+    void reorderList(ListNode* head) {
+        if(!head || !head -> next) return;
+        auto mid = middleNode(head);
+        auto R = reverseList(mid), L = head -> next;
+        for(int i = 0; L != R; i++, head = head -> next)     // re-order in alternating fashion 
+            if(i & 1) {                              
+                head -> next = L;
+                L = L -> next;
+            }
+            else {
+                head -> next = R;
+                R = R -> next;
+            }
     }
 };
