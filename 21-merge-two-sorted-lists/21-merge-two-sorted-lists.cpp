6d8021d631d618f037265bda653a4eb6a7f24023
jatin1322 <75074656+jatin1322@users.noreply.github.com>
2022-06-22 22:34:04 +0530

    Time: 11 ms (58.59%), Space: 14.9 MB (14.48%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -28,8 +28,8 @@ public:
             temp=temp->next;
             
         }
-        while(list1){ temp->next=list1; list1=list1->next ; temp=temp->next; }
-         while(list2){ temp->next=list2; list2=list2->next ; temp=temp->next; }
+        if(list1)temp->next=list1; 
+         if(list2) temp->next=list2; 
         return dummy->next;
     }
 };
