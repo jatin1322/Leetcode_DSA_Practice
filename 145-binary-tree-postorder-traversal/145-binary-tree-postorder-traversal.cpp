9828fa10705e780f5915a7307eb1aa6d7a60d1a5
jatin1322 <75074656+jatin1322@users.noreply.github.com>
2022-07-05 19:39:21 +0530

    Time: 2 ms (61.90%), Space: 8.4 MB (63.48%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
@@ -16,26 +16,60 @@
  
 // using  2 stacks 
 
+//   vector<int> postorderTraversal(TreeNode* root) {
+//          vector<int> ans;
+//         if(root==NULL)return ans;
+//        stack<TreeNode*> s1,s2;
+//         s1.push(root);
+//         while(!s1.empty()){
+//             root=s1.top();
+//             s2.push(root); s1.pop();
+//             if(root->left)s1.push(root->left);
+//                         if(root->right)s1.push(root->right);
+           
+            
+//         }
+//         while(!s2.empty()){
+//             ans.push_back(s2.top()->val);
+//             s2.pop();
+//         }
+//         return ans;
+
+// using 1 stack tricy one
+
 class Solution {
 public:
      
     vector<int> postorderTraversal(TreeNode* root) {
          vector<int> ans;
         if(root==NULL)return ans;
-       stack<TreeNode*> s1,s2;
-        s1.push(root);
-        while(!s1.empty()){
-            root=s1.top();
-            s2.push(root); s1.pop();
-            if(root->left)s1.push(root->left);
-                        if(root->right)s1.push(root->right);
+       stack<TreeNode*> st;
+        
+        TreeNode* curr=root;
+        while(curr!=NULL|| !st.empty()){
+            
+           if(curr!=NULL){
+               st.push(curr);
+               curr=curr->left;
+           }
+        else{
+           TreeNode* temp=st.top()->right;
+            if(temp==NULL){
+                temp=st.top(); st.pop();
+                ans.push_back(temp->val);
+                while(!st.empty() && temp==st.top()->right){
+                     temp=st.top(); st.pop();
+                ans.push_back(temp->val);
+                }
+            }
+            else {
+                curr=temp;
+            }
+        }
            
             
         }
-        while(!s2.empty()){
-            ans.push_back(s2.top()->val);
-            s2.pop();
-        }
+       
         return ans;
     }
 };
