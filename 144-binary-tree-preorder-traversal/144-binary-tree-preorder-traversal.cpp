5a5de9fb8b0efcec11b350f788fea28008b3d824
jatin1322 <75074656+jatin1322@users.noreply.github.com>
2022-07-05 18:01:18 +0530

    Time: 2 ms (62.10%), Space: 8.3 MB (77.19%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
@@ -9,25 +9,51 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-public:
-    void preorder(TreeNode* root,vector<int> &v){
-         if(root==NULL){
-            return ;
-        }
+// recursive
+
+// class Solution {
+// public:
+//     void preorder(TreeNode* root,vector<int> &v){
+//          if(root==NULL){
+//             return ;
+//         }
        
-        v.push_back(root->val);
-        preorder(root->left,v);
-        preorder(root->right,v);
+//         v.push_back(root->val);
+//         preorder(root->left,v);
+//         preorder(root->right,v);
        
         
-    }
-    vector<int> preorderTraversal(TreeNode* root) {
-         vector<int> v;
+//     }
+//     vector<int> preorderTraversal(TreeNode* root) {
+//          vector<int> v;
        
-       preorder(root,v);
+//        preorder(root,v);
       
-      return v;  
+//       return v;  
       
-    }
+//     }
+// };
+
+//iterative
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+    vector<int> ans;
+        if(root==NULL){
+         return ans;
+        }
+        stack<TreeNode*> s;
+        s.push(root);
+        
+        while(!s.empty()){
+            TreeNode* node=s.top();
+            s.pop();
+            if(node->right){s.push(node->right);}
+             if(node->left){s.push(node->left);}
+            ans.push_back(node->val);
+        }
+        return ans;
+        
+     }
 };
