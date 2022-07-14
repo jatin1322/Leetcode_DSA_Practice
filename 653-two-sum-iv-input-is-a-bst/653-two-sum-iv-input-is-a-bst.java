87b3ad8b6909fe7b11dbf5e7d8c2c9d8d569f851
jatin1322 <75074656+jatin1322@users.noreply.github.com>
2022-07-14 19:31:51 +0530

    Time: 3 ms (95.20%), Space: 50.4 MB (73.77%) - LeetHub

---
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
@@ -0,0 +1,52 @@
+class Solution {
+    public boolean findTarget(TreeNode root, int k) {
+        if (root == null) {
+            return false;
+        }
+        TreeNode start = root;
+        TreeNode end = root;
+        while (start.left != null) {
+            start = start.left;
+        }
+        while (end.right != null) {
+            end = end.right;
+        }
+        while (start != end) {
+            int sum = start.val + end.val;
+            if (sum > k) {
+                end = findPredecessor(root, end);
+            } else if (sum < k) {
+                start = findSuccessor(root, start);
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+    private TreeNode findPredecessor(TreeNode root, TreeNode node) {
+        TreeNode pre = null;
+        TreeNode cur = root;
+        while (cur != null) {
+            if (cur.val < node.val) {
+                pre = cur;
+                cur = cur.right;
+            } else {
+                cur = cur.left;
+            }
+        }
+        return pre;
+    }
+    private TreeNode findSuccessor(TreeNode root, TreeNode node) {
+        TreeNode succ = null;
+        TreeNode cur = root;
+        while (cur != null) {
+            if (cur.val > node.val) {
+                succ = cur;
+                cur = cur.left;
+            } else {
+                cur = cur.right;
+            }
+        }
+        return succ;
+    }
+}
