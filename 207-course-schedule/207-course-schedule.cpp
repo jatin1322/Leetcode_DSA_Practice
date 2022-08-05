c02e87188011ec1a30d15fd3d0b5bd0e2a35a8ba
jatin1322 <75074656+jatin1322@users.noreply.github.com>
2022-08-05 16:04:01 +0530

    Time: 22 ms (90.82%), Space: 13.7 MB (64.66%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
-    bool canFinish(int n, vector<vector<int>>& pr) {
-        vector<vector<int>> adj(n,vector<int>());
-        vector<int> indegree(n,0);
-        vector<int> topo;
-        for(int i=0; i<pr.size();i++){
-            adj[pr[i][1]].push_back(pr[i][0]);
-            indegree[pr[i][0]]++;
+    bool checkCycle(vector<int> adj[], int i, vector<int> &vis, vector<int> &dfsvis){
+        vis[i] = 1;
+        dfsvis[i] = 1;
+        for(int u : adj[i]){
+            if(!vis[u]){
+                if(checkCycle(adj, u, vis, dfsvis)) return true;
+            }
+            else if(dfsvis[u]) return true;
         }
-        queue<int> q;
-        for(int i=0;i<n;i++){
-            if(indegree[i]==0){q.push(i); topo.push_back(i);}
+        dfsvis[i] = 0;
+        return false;
+    }
+    bool canFinish(int V, vector<vector<int>>& pr) {
+        // Build graph
+        vector<int> adj[V];
+        for(int i = 0; i<pr.size(); ++i){
+            int u = pr[i][0];
+            int v = pr[i][1];
+            adj[v].push_back(u);
         }
-        while(!q.empty()){
-            int node=q.front();
-            q.pop();
-            for(auto it:adj[node]){
-                indegree[it]--;
-                if(indegree[it]==0){
-                    q.push(it);
-                    topo.push_back(it);
-                }
+        // check whether graph is DAG or not
+        vector<int> visited(V, 0);
+        vector<int> dfsvis(V, 0);
+        for(int i = 0; i<V; ++i){
+            if(!visited[i]){
+                if(checkCycle(adj, i, visited, dfsvis)) return false;
             }
         }
-        return topo.size()==n;
-        
-        }
+        return true;
+    }
 };
