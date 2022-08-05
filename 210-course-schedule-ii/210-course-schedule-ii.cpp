750994e1ecb525c6d5b5727252dbd8d5dcdbc20f
jatin1322 <75074656+jatin1322@users.noreply.github.com>
2022-08-05 16:08:17 +0530

    Time: 45 ms (27.50%), Space: 13.3 MB (86.08%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& pr) {
+        
+            vector<vector<int>> adj(n,vector<int>());
+        vector<int> indegree(n,0);
+        vector<int> topo;
+        for(int i=0; i<pr.size();i++){
+            adj[pr[i][1]].push_back(pr[i][0]);
+            indegree[pr[i][0]]++;
+        }
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){q.push(i); topo.push_back(i);}
+        }
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                    topo.push_back(it);
+                }
+            }
+        }
+        if(topo.size()==n) return topo;
+        else {topo.clear(); return topo;}
+        
+        
+        }
+    
+};
