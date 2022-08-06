425f5eeda07e8782108bc3732dbd8ba50324ff66
jatin1322 <75074656+jatin1322@users.noreply.github.com>
2022-08-06 18:41:29 +0530

    Time: 38 ms (9.83%), Space: 9.6 MB (22.00%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.cpp
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-vector<int> findRedundantConnection(vector<vector<int>>& e) {
-	int n = size(e);
-	vector<vector<int>> graph(n+1);
-	vector<bool> vis(n+1);                
-	for(auto& E : e) {
-        fill(begin(vis), end(vis), false);     // reset the vis array
-        graph[E[0]].push_back(E[1]), graph[E[1]].push_back(E[0]);
-        if(dfs(graph, vis, E[0])) return E;
+    bool dfs(vector<int> adj[],vector<int> &vis,int cur,int par=-1){
+        if(vis[cur]) return true;
+        vis[cur]=1;
+        for(auto it:adj[cur]){
+            if(dfs(adj,vis,it,cur) && it!=par) return true;
+        }
+        return false;
     }
-	return { };    // un-reachable
-}
-bool dfs(vector<vector<int>>& graph, vector<bool>& vis, int cur, int par = -1) {
-    if(vis[cur]) return true;    // reached already visited node -  cycle detected
-    vis[cur] = true;
-    for(auto child : graph[cur]) 
-        if(child != par && dfs(graph, vis, child, cur)) return true;
-    return false;    // no cycle found
-}
-
-};
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n=edges.size();
+        vector<int> adj[n+1];
+        vector<int> vis(n+1,false);
+        for(auto it:edges){
+            fill(vis.begin(),vis.end(),false);
+            adj[it[0]].push_back(it[1]);
+             adj[it[1]].push_back(it[0]);
+            if(dfs(adj,vis,it[0])) return it;
+        }
+        return { };
+    }
+ };
