becea61bcd12ab3816a80df97e02c3a9db6dfeff
jatin1322 <75074656+jatin1322@users.noreply.github.com>
2022-08-07 17:28:38 +0530

    Time: 108 ms (99.69%), Space: 39.9 MB (74.26%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,62 +1,39 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int,int>>> adj(n+1);
-     priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-    
-        for(auto it:times){
-            int a=it[0];
-            int b=it[1];
-            int c=it[2];
-            adj[a].push_back({b,c});
-        }
-        vector<int> dist(n+1,INT_MAX);
-        vector<int> vis(n+1,0);
-        vector<int>parent(n+1,-1);
-
-        parent[k]=k;
-        dist[k]=0;
-        vis[k]=1;
-  for(int i=0;i<adj[k].size();i++){
-            pq.push({adj[k][i].second,adj[k][i].first});
-            if(dist[k]+adj[k][i].second<dist[adj[k][i].first]){
-                  dist[adj[k][i].first]=  dist[k]+adj[k][i].second;
-                    parent[adj[k][i].first]=k;
-                }
+        
+     vector<pair<int,int>> adj[n+1];
+        for(int i=0;i<times.size();i++){
+            adj[times[i][0]].push_back({times[i][1],times[i][2]});
+            
         }
         
-        while(pq.empty()!=true){
-           pair<int, int> cur=pq.top();
-    pq.pop();
-            // cout<<cur.second<<cur.first<<endl;
-           if(vis[cur.second ]==0){
-           
-            for(int i=0;i<adj[cur.second].size();i++){
-                // cout<<graph[cur.second][i].first<<endl;
-                  if(vis[adj[cur.second][i].first]==0){
-                if(dist[cur.second]+adj[cur.second][i].second<dist[adj[cur.second][i].first]){
-                  dist[adj[cur.second][i].first]=  dist[cur.second]+adj[cur.second][i].second;
- pq.push({  dist[adj[cur.second][i].first],adj[cur.second][i].first});
-                    parent[adj[cur.second][i].first]=cur.second;
-                      }
+        vector<int> dist(n+1,1e9);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        dist[k]=0;
+        pq.push({0,k});
+        while(!pq.empty()){
+            int distt=pq.top().first;
+            int prev=pq.top().second;
+            pq.pop();
+            for(pair<int, int> it:adj[prev]){
+                int next=it.first;
+                int nextdist=it.second;
+                if(dist[next]>dist[prev]+nextdist){
+                    dist[next]=dist[prev]+nextdist;
+                    pq.push({dist[next],next});
                 }
             }
-            }
-            vis[cur.second]=1;
         }
-        int m=INT_MIN;
+        int ans=-1;
         for(int i=1;i<dist.size();i++){
-            // cout<<dist[i]<<" ";
-            if(m<dist[i]){
-                m=dist[i];
+            if(dist[i]==1e9){
+                return -1;
             }
+            ans=max(ans,dist[i]);
+            
         }
-        if(m!=INT_MAX){
-            return m;
-        }
-        else{
-            return -1;
-        }
-
+        return ans;
     } 
+    
 };
