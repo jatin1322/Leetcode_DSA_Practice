c37415fc3f5e81a9bcd813d4784c39fee3ef22d8
jatin1322 <75074656+jatin1322@users.noreply.github.com>
2022-08-10 10:42:27 +0530

    Time: 975 ms (8.40%), Space: 153.7 MB (6.01%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
-    int f(int ind,int target,vector<int> &arr,vector<vector<int>> &dp){
+//     int f(int ind,int target,vector<int> &arr,vector<vector<int>> &dp){
        
-        if(target==0) return 0;
-        if(ind==0) return target;
-        if(dp[ind][target]!=-1) return dp[ind][target];
-        int take=1e9;
-     if(target>=arr[ind]) take =1+f(ind,target-arr[ind],arr,dp);
-       // int nottake=1e9;
-        int nottake=f(ind-1,target,arr,dp);
-        return dp[ind][target]=min(take,nottake);
-    }
+//         if(target==0) return 0;
+//         if(ind==0) return target;
+//         if(dp[ind][target]!=-1) return dp[ind][target];
+//         int take=1e9;
+//      if(target>=arr[ind]) take =1+f(ind,target-arr[ind],arr,dp);
+//        // int nottake=1e9;
+//         int nottake=f(ind-1,target,arr,dp);
+//         return dp[ind][target]=min(take,nottake);
+//     }
+    
+    
+    
     int numSquares(int n) {
         vector<int> perfect;
         for(int i=0;i<n;i++){
@@ -18,8 +21,20 @@ public:
             else break;
         }
         int ind=perfect.size();
-        vector<vector<int>> dp(ind,vector<int> (n+1,-1));
+        vector<vector<int>> dp(ind,vector<int> (n+1,0));
+        for(int i=0;i<ind;i++){
+           
+            for(int j=0;j<=n;j++){
+                if(j==0){dp[i][j]=0; continue;}
+                if(i==0) {dp[i][j]=j;continue;}
+                 int take=1e9;
+     if(j>=perfect[i]) take =1+dp[i][j-perfect[i]];
         
-        return f(ind-1,n,perfect,dp);
+       int nottake=  dp[i-1][j];
+         dp[i][j]=min(take,nottake);
+                
+            }
+        }
+        return dp[ind-1][n];
     }
 };
