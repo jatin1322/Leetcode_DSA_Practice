8bd9bbba40755800391ceaa2bff9c182dfa158f8
jatin1322 <75074656+jatin1322@users.noreply.github.com>
2022-08-14 17:27:06 +0530

    Time: 78 ms (28.30%), Space: 19 MB (84.66%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -8,12 +8,12 @@ public:
         int l=arr[0][0]; int r=arr[0][1];
         for(int i=1;i<n;i++){
             if(arr[i][0]>=l && arr[i][1] <= r){
-                l=min(l,min(arr[i][0],arr[i-1][0]));
-                r=max(r,max(arr[i][1],arr[i-1][1]));
+                 l=min(l,arr[i][0]);
+                r=max(r,arr[i][1]);
             }
            else if(arr[i][0]<=r){
-                l=min(l,min(arr[i][0],arr[i-1][0]));
-                r=max(r,max(arr[i][1],arr[i-1][1]));
+                l=min(l,arr[i][0]);
+                r=max(r,arr[i][1]);
                
             }
             
