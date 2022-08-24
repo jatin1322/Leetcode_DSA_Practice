99cf2bcf7ae0e3dc487bb0325d2fd39a92bb41b0
jatin1322 <75074656+jatin1322@users.noreply.github.com>
2022-08-24 14:27:56 +0530

    Time: 10 ms (72.80%), Space: 10.9 MB (97.07%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void solve(int ind,int target,vector<int> &arr,vector<int> &temp,vector<vector<int>> &ans){
+        
+        // pick
+        if(ind>=arr.size()){
+            if(target==0)
+            ans.push_back(temp);
+            return;
+        }
+        
+         
+       if(target>=arr[ind]){ temp.push_back(arr[ind]);
+           solve(ind,target-arr[ind],arr,temp,ans);
+       
+        
+        temp.pop_back();}
+        // not pick
+        solve(ind+1,target,arr,temp,ans);
+        
+        
+    }
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        sort(nums.begin(),nums.end());
+        solve(0,target,nums,temp,ans);
+        return ans;
+    }
+};
