3db61be0d85251a841f60651b6ce099e9326620e
jatin1322 <75074656+jatin1322@users.noreply.github.com>
2022-09-10 13:27:14 +0530

    Time: 7 ms (42.23%), Space: 6.6 MB (80.90%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -1,29 +1,49 @@
-class Solution {
-public:
-    string removeOuterParentheses(string s) {
-        string ans="";
-        int left=0;
-        int right=0;
-        int n=s.size();
-        int i=0;
-        string temp="";
-        while(i<n){
-            if(left==0 && right ==0 && s[i]=='('){
-                left++;
-            }
-            else if(s[i]=='(') {left++; temp+='(';}
-            else if(s[i]==')'){ right++;  temp+=')';}
+// class Solution {
+// public:
+//     string removeOuterParentheses(string s) {
+//         string ans="";
+//         int left=0;
+//         int right=0;
+//         int n=s.size();
+//         int i=0;
+//         string temp="";
+//         while(i<n){
+//             if(left==0 && right ==0 && s[i]=='('){
+//                 left++;
+//             }
+//             else if(s[i]=='(') {left++; temp+='(';}
+//             else if(s[i]==')'){ right++;  temp+=')';}
             
-            if(left==right){
+//             if(left==right){
                 
-                temp.pop_back();
-                ans+=temp;
-                temp="";
-                right=0;
-                left=0;
+//                 temp.pop_back();
+//                 ans+=temp;
+//                 temp="";
+//                 right=0;
+//                 left=0;
+//             }
+//             i++;
+//         }
+//         return ans;
+//     }
+    class Solution {
+public:
+    string removeOuterParentheses(string S) {
+        int count = 0;
+        std::string str;
+        for (char c : S) {
+            if (c == '(') {
+                if (count++) {
+                    str += '(';
+                }
+            } else {
+                if (--count) {
+                    str += ')';
+                }
             }
-            i++;
         }
-        return ans;
+        return str;
     }
 };
+
+// };
