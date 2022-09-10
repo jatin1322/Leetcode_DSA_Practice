42038124d8fce4587b5613e7428da5bbf47c0c8e
jatin1322 <75074656+jatin1322@users.noreply.github.com>
2022-09-10 13:17:53 +0530

    Time: 8 ms (34.23%), Space: 6.6 MB (95.19%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string ans="";
+        int left=0;
+        int right=0;
+        int n=s.size();
+        int i=0;
+        string temp="";
+        while(i<n){
+            if(left==0 && right ==0 && s[i]=='('){
+                left++;
+            }
+            else if(s[i]=='(') {left++; temp+='(';}
+            else if(s[i]==')'){ right++;  temp+=')';}
+            
+            if(left==right){
+                
+                temp.pop_back();
+                ans+=temp;
+                temp="";
+                right=0;
+                left=0;
+            }
+            i++;
+        }
+        return ans;
+    }
+};
