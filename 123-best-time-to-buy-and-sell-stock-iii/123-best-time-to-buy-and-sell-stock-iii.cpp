8e2b6dccd1c22447920f93aa38fc4be48e691c9d
jatin1322 <75074656+jatin1322@users.noreply.github.com>
2022-09-10 17:50:48 +0530

    Time: 345 ms (58.42%), Space: 75.2 MB (87.09%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
-    int solve(int ind,int buy,int cap,vector<int> &arr,vector<vector<vector<int>>> &dp){
-       if(cap==0) return 0;
-        if(ind==arr.size()) return 0;
-        if(dp[ind][buy][cap]!=-1) return dp[ind][buy][cap];
-        int profit;
-        if(buy==0){
-           profit=max(solve(ind+1,0,cap,arr,dp),solve(ind+1,1,cap,arr,dp)-arr[ind]); 
-        }
-        else profit=max(arr[ind]+solve(ind+1,0,cap-1,arr,dp),solve(ind+1,1,cap,arr,dp));
-        return dp[ind][buy][cap]=profit;
-    }
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        vector<vector<vector<int>>> dp(n,vector<vector<int>>(2,vector<int> (3,-1)));
-        
-        return solve(0,0,2,prices,dp);
+        int firstBuy = INT_MAX, secondBuy = INT_MAX, firstProfit = 0, secondProfit = 0;
+        for(int currPrice: prices) {
+            firstBuy = min(firstBuy, currPrice);
+            firstProfit = max(firstProfit, currPrice - firstBuy);
+            secondBuy = min(secondBuy, currPrice - firstProfit);
+            secondProfit = max(secondProfit, currPrice - secondBuy);
+        }
+        return secondProfit;
     }
 };
